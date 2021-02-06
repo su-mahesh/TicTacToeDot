7f14fd255f44b38d98669784c5ce3983ce41fac0
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-06 11:24:19 +0530

    [MaheshKangude] ADDED: FreshTicTacToeBoard

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,11 +2,51 @@
 
 namespace TicTacToe
 {
-    class Program
+    class TicTacToe
     {
-        static void Main(string[] args)
+		private const int boardSize = 10;
+		private char computerLetter = ' ';
+		private char palyerLetter = ' ';
+		private char[] board = new char[boardSize];
+
+		TicTacToe()
+		{
+			for (int i = 0; i < boardSize; i++)
+				board[i] = ' ';
+            Console.WriteLine("**Game started**");
+		}
+		public void ShowBoard()
+		{
+			int charIndex = 1;
+
+            Console.WriteLine("");
+			for (int i = 1; i <= 3; i++)
+			{
+				for (int j = 1; j <= 3; j++)
+				{
+                    Console.Write(" " + board[charIndex] + " ");
+					charIndex++;
+					if (j != 3)
+						Console.Write("|");
+				}
+				Console.WriteLine("");
+
+				for (int j = 1; j <= 11 && i != 3; j++)
+					Console.Write("-");
+
+				Console.WriteLine("");
+			}
+			Console.WriteLine("");
+		}
+
+		static void Main(string[] args)
         {
             Console.WriteLine("TicTacToe");
-        }
+
+            TicTacToe TicTacToeGame = new TicTacToe();
+			TicTacToeGame.ShowBoard();
+
+
+		}
     }
 }
